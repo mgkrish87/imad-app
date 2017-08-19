2f46711998ef8ec2bb2771435aff708fc78316e7
mgkrish87 <30500897+mgkrish87@users.noreply.github.com>
2017-08-19 15:26:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -68,12 +68,12 @@ app.get('/article-one', function(req,res) {
     res.send(createTemplete(articleOne));
 });
 
-app.get('/Article-two', function(req,res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+app.get('/article-one', function(req,res) {
+    res.send(createTemplete(articletwo));
 });
 
-app.get('/Article-three', function(req,res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+app.get('/article-three', function(req,res) {
+    res.send(createTemplete(articlethree));
 });
 
 app.get('/', function (req, res) {
