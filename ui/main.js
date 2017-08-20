1b68f601c2997b7bac988bf391b1964d9c08bf30
mgkrish87 <30500897+mgkrish87@users.noreply.github.com>
2017-08-20 08:40:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,17 @@
 console.log('Loaded!');
+
+var button = document.getElementById("counter");
+var counter = 0;
+button.onClick = funciton(){
+    
+    //Make a request to end point
+    
+    //Get the variable.
+    
+    //show it in correct span
+    
+    counter = counter+1;
+    var span = document.getElementById("span");
+    span.innerHTML = counter.toString();
+    
+}
