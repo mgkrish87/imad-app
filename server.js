dbd7b2f2c3aab7f56b108462775c6c68a2722fd8
mgkrish87 <30500897+mgkrish87@users.noreply.github.com>
2017-08-20 22:03:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var crypto = require('crypto');
 
 var app = express();
 app.use(morgan('combined'));
@@ -67,6 +68,17 @@ return htmlTemplete;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+function hash(input,salt){
+    var hashed = crypto.pbkdf2Sync(input, salt, 100000, 512, 'sha512');
+    return hashed.toString('hex');
+}
+
+app.get('/hash/:input', function(req,res) {
+    var hashstring = hash(req.params.input,'this-is-some-string');
+    res.send(hasstring);
+});
+
 var counter = 0;
 app.get('/counter', function(req,res){
    counter = counter + 1;
