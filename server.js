70a55a5a250aeefebd734cac70f13242a8ba86ce
mgkrish87 <30500897+mgkrish87@users.noreply.github.com>
2017-08-22 01:05:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -91,6 +91,17 @@ app.get('/hash/:input', function(req,res) {
 });
 
 var pool = new Pool(config);
+app.get('/test-db', function(req,res) {
+    pool.query('SELECT * FROM test', function(err, result){
+        if(err){
+            res.status(500).send(err.toString());
+        }else{
+            res.send(JSON.stringify(result));
+        }
+    });
+});
+
+
 app.post('/create-user', function(req,res){
     
     var username = req.body.username;
