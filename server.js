52052df6067def25b66aab1cf3ca636cb0a6954e
mgkrish87 <30500897+mgkrish87@users.noreply.github.com>
2017-08-21 22:26:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ app.get('/', function (req, res) {
 
 function hash(input,salt){
     var hashed = crypto.pbkdf2Sync(input, salt, 100000, 512, 'sha512');
-    return ['pbkdf2','100000','salt',hashed.toString('hex')].join('$');
+    return ["pbkdf2","100000",salt,hashed.toString('hex')].join('$');
 }
 
 app.get('/hash/:input', function(req,res) {
