0c8bcd9beb0aa51409b327dba64544212f10769a
mgkrish87 <30500897+mgkrish87@users.noreply.github.com>
2017-09-22 23:18:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,12 +17,74 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 
+var articles = {
+    'article-one': {
+        title: "Article One | Gopal",
+        heading: "My First Article",
+        Content: `<p>
+                        This is my first Article and I love to build webapps.
+                </p>`
+    },
+    'article-two': {
+        title: "Article two | Gopal",
+        heading: "My Second Article",
+        Content: `<p>
+                        This is my Second Article and I love to build webapps.
+                </p>`
+    },
+    'article-three': {
+        title: "Article Three | Gopal",
+        heading: "My Third Article",
+        Content: `<p>
+                        This is my Third Article and I love to build webapps.
+                </p>`
+    }
+};
+
+function createTemplete(data){
+  var title = data.title;
+  var heading = data.heading;
+  var Content = data.Content;
+
+var htmlTemplete = `
+<html>
+    <head>
+        
+    <title>
+    ${title}
+    </title>
+    <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    
+    <body>
+        <div class="container">
+            <div>
+                <a href="/"> Home </a>
+            </div>
+            <h3>${heading}</h3>
+            
+            <div>
+                Aug 19th 2017.
+            </div>
+            <div>
+                ${Content}
+            </div>
+        </div>
+    </body>
+</html>
+`;
+return htmlTemplete;
+}
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req, res) {
-  res.send('Article one is requested');
+
+app.get('/:articleName', function (req, res) {
+    var articleName = req.params.articleName;
+    res.send(createTemplate(articles[articleName]));
 });
 
 
