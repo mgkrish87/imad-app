2961e21aa9a990df9e678a7cfca913d634c901f2
mgkrish87 <30500897+mgkrish87@users.noreply.github.com>
2017-09-23 17:02:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -89,7 +89,11 @@ return htmlTemplete;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+var counter = 0;
+app.get('/counter' , function(req,res){
+    counter = counter+1;
+    res.send(counter);
+});
 
 app.get('/:articleName', function (req, res) {
     var articleName = req.params.articleName;
