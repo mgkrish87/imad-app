1330db5a61de3d848cf8cd609beeb53f5928aba3
mgkrish87 <30500897+mgkrish87@users.noreply.github.com>
2017-09-23 12:45:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -101,8 +101,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/ui/approve.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'approve.png'));
 });
 
 app.get('/ui/main.js', function (req, res) {
