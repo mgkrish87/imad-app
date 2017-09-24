f0d9846d6f1e08d5745e29f85e65457fdef2553f
mgkrish87 <30500897+mgkrish87@users.noreply.github.com>
2017-09-24 09:12:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,7 @@ submitbutton.onclick = function(){
   var names = ['name1', 'name2', 'name3'];
   var list = '';
   for (var i=0; i< names.length; i++){
-      list == '<li>' + names[i] + '</li>';
+      list += '<li>' + names[i] + '</li>';
   }
   var ul = document.getElementById('orderlist');
   ul.innerHTML = list;
