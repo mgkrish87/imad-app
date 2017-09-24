56ce85fe2af63b284de96095a14c5ffe8050bca8
mgkrish87 <30500897+mgkrish87@users.noreply.github.com>
2017-09-24 09:41:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,21 +7,21 @@ submitbutton.onclick = function(){
             if(request.readyState === XMLHttpRequest.DONE) {
                 if(request.status === 200)
                 {
-            var names = request.responseText;
-            names = JSON.parse(names);
-            var list = '';
-                for (var i=0; i< names.length; i++){
+                    var names = request.responseText;
+                    names = JSON.parse(names);
+                    var list = '';
+                    for (var i=0; i< names.length; i++){
                         list += '<li>' + names[i] + '</li>';
                         }
                  var ul = document.getElementById('orderlist');
-                ul.innerHTML = list;
+                 ul.innerHTML = list;
                 }
             }
     
         };
      var nameInput = document.getElementById('name');
      var name = nameInput.value;        
-     request.open('GET', 'http://marrigopal87.imad.hasura-app.io/submit-name?name' +name, true);
+     request.open('GET', 'http://marrigopal87.imad.hasura-app.io/submit-name?name=' +name, true);
      request.send(null);
 };
 
